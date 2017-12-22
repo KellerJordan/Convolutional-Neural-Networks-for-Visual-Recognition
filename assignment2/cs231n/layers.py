fix running mean and variance in batchnorm layer, complete batchnorm notebook

assignment2/cs231n/layers.py
@@ -170,13 +170,14 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # variance, storing your result in the running_mean and running_var   #
         # variables.                                                          #
         #######################################################################
-        scale = 1 / np.sqrt(x.var(axis=0) + eps)
-        x_norm = (x - x.mean(axis=0)) * scale
+        x_mean, x_var = x.mean(axis=0), x.var(axis=0)
+        scale = 1 / np.sqrt(x_var + eps)
+        x_norm = (x - x_mean) * scale
         out = gamma * x_norm + beta
         cache = (x_norm, scale, gamma)
         
-        running_mean = momentum * running_mean + (1 - momentum) * x.mean()
-        running_var = momentum * running_var + (1 - momentum) * x.var()
+        running_mean = momentum * running_mean + (1 - momentum) * x_mean
+        running_var = momentum * running_var + (1 - momentum) * x_var
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -187,7 +188,7 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # then scale and shift the normalized data using gamma and beta.      #
         # Store the result in the out variable.                               #
         #######################################################################
-        x_norm = (x - running_mean) / (np.sqrt(running_var + eps))
+        x_norm = (x - running_mean) / np.sqrt(running_var + eps)
         out = gamma * x_norm + beta
         #######################################################################
         #                          END OF YOUR CODE                           #